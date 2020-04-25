2abcdea1ef08af463ead4ed196597f23198eb46e
Ipeleng Mekgwe <ipelengm@dpdlaser.co.za>
2020-04-25 22:15:13 +0200

    added migrations, makefile

---
+++ SolarCoffee.Data/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [MaxLength(50)]
+        public string Cell { get; set; }
+
+        public CustomerAddress PrimaryAddress { get; set; }
+    }
+}
